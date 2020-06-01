f17955dd486d859a8fa5ba87aeaea4f83838c08b
Mike Jones <michael.jones@domo.com>
2020-06-01 12:29:07 -0600

    SugarForce Step 1

---
+++ js/leads.js
@@ -0,0 +1,13 @@
+// Some DataSets are massive and will bring any web browser to its knees if you
+// try to load the entire thing. To keep your app performing optimally, take
+// advantage of filtering, aggregations, and group by's to bring down just the
+// data your app needs. Do not include all columns in your data mapping file,
+// just the ones you need.
+//
+// For additional documentation on how you can query your data, please refer to
+// https://developer.domo.com/docs/dev-studio/dev-studio-data
+domo.get('/data/v1/leads?limit=10')
+    .then(function(leads){
+      console.log("leads", leads);
+    });
+
---
+++ js/reports.js
@@ -0,0 +1,2 @@
+// For more on how to use Phoenix view the documentation at:
+// https://domoapps.github.io/domo-phoenix/
